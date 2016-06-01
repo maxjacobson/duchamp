1500f5db8af8c2f06d816e8fe57e4d3cdcdb6433
Max Jacobson <max@hardscrabble.net>
2016-06-01 15:33:52 -0400

    introduce some rubocop offenses

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 RSpec.describe "foo" do
-  it "exist" do
+  # this is a convention style offense (single quotes):
+  it 'exist' do
+    # this is a lint offense (unused variable):
+    fafa = 47
     expect(true).to eq true
   end
 end
